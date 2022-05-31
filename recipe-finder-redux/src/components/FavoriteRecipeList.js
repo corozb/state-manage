ef58f3ce80a702774c59cbc5aa6d1396340a7dc1
cristian.orozco <cristian.orozco@globant.com>
2022-05-31 12:35:09 -0500

    simple redux ecommerce

--- recipe-finder-redux/src/components/FavoriteRecipeList.js
+++ recipe-finder-redux/src/components/FavoriteRecipeList.js
@@ -1,25 +1,22 @@
-import { useSelector } from "react-redux"
-import RecipeItem from "./RecipeItem"
-import {Link} from 'react-router-dom'
-
+import { useSelector } from 'react-redux'
+import RecipeItem from './RecipeItem'
+import { Link } from 'react-router-dom'
 
 const FavoriteRecipeList = () => {
-  const {favoriteRecipes} = useSelector(state => state)
+	const { favoriteRecipes } = useSelector((state) => state)
 
-  return (
-    <>
-    <h4 className="link">
-      <Link to='/'>Home</Link>
-    </h4>
-      <h4>Favorite Recipes:</h4>
-      {
-        favoriteRecipes.map((recipe, index) => (
-          <RecipeItem key={index} recipe={recipe} favoriteButton={false}/>
-        ))
-      }
-    </>
-  )
+	return (
+		<>
+			<h4 className='link'>
+				<Link to='/'>Home</Link>
+			</h4>
+
+			<h4>Favorite Recipes:</h4>
+			{favoriteRecipes.map((recipe, index) => (
+				<RecipeItem key={index} recipe={recipe} favoriteButton={false} />
+			))}
+		</>
+	)
 }
-
 
 export default FavoriteRecipeList
